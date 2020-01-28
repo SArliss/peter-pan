e29193ffad564840007d2a79227ca673881c9f63
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2020-01-28 13:00:59 -0500

    final changes before surging

--- peter-pan-app/src/components/Contact.js
+++ peter-pan-app/src/components/Contact.js
@@ -7,16 +7,17 @@ const Contact = () => {
     <div className="contact-wrapper" id="contact">
       <h1 className="contact-header">CONTACT US</h1>
       <p className="for-all-ques">
-        For all orders, questions  and press inquiries, you can reach us
-        at <a href="tel:1-718-389-3676">(718) 389-3676</a> between 8AM to 4PM daily
+        For all orders, questions and press inquiries, you can reach us at{" "}
+        <a href="tel:1-718-389-3676">(718) 389-3676</a> between 8AM to 4PM daily
       </p>
       <p className="we-love">
-        We'd love to hear from you! Show us some love on our <a href="https://www.facebook.com/peterpandonuts/">social media</a>
+        We'd love to hear from you! Show us some love on our{" "}
+        <a href="https://www.facebook.com/peterpandonuts/">social media</a>
       </p>
       <div className="img-wrapper">
-        <img src={banner} />
-        <img src={donutGif} />
-        <img src={donutGif} />
+        <img src={donutGif} alt="Donut GIF"/>
+        <img src={banner} alt="peter-pan-banner"/>
+        <img src={donutGif} alt="Donut GIF" />
       </div>
     </div>
   );
